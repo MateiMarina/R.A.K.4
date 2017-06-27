b210ac34b530316c2c649f4f6bc2af02c2093333
Matei Marina <i.marina.16@aberdeen.ac.uk>
2017-06-27 11:30:49 +0100

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
+  
 
 # before_save :format_name
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  devise_for :admins
+  
   root 'static_pages#home'
   get 'users/new'
   get 'users/show'
