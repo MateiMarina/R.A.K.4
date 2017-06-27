14cca69e32a88f011a37b83c01b1fdef7be25e16
Matei Marina <i.marina.16@aberdeen.ac.uk>
2017-06-27 14:42:33 +0100

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-       flash[:success] = "Welcome to Archer Knight!"
+      log_in @user
+      flash[:success] = "Welcome to Archer Knight!"
       redirect_to @user
     else
       render 'new'
