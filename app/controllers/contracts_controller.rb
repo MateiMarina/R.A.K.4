98f8a929d742fbba2f8581a00172a2e99e8c796e
tonyunique2006@yahoo.com <tonyunique2006@yahoo.com>
2017-07-19 10:52:53 +0000

    New

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,22 +1,61 @@
 class ContractsController < ApplicationController
-    # def index
-    # end
-  
-    # def show
-    # end
-  
-    # def new
-    # end
-  
-    # def create
-    # end
-  
-    # def edit
-    # end
-  
-    # def update
-    # end
-  
-    # def destroy
-    # end
+      
+      #showing all user
+    def index
+    @contract=Contract.all 
+    
+    end
+            
+            
+    def show
+    @contract = Contract.find(params[:id])
+    end
+        
+        #display form 
+    def new
+    @contract=Contract.new
+    end
+         #creating new comany record
+    def create
+     @contract=Contract.new(located)
+    if  @contract.save
+        flash[:success] = "A record has been successfully deleted"
+    redirect_to contracts_path
+    else
+      render 'new'
+    end
+    end
+
+        #strong parameters
+    def located
+    params.require(:contract).permit(:contract_name, :contract_status, :services_rendered, :contract_value, :award_year)
+    end
+    
+
+    
+     def edit
+        @contract=Contract.find(params[:id])
+     end
+     
+     
+     #upadat user  
+      def update
+        @contract=Contract.find(params[:id])
+        if @contract.update_attributes(located)
+           flash[:success] = "Contract form updated"
+           redirect_to contracts_path
+        else
+           render'edit'
+        end
+      end
+
+    
+    
+     #deleting a record
+    def destroy
+    Contract.find(params[:id]).destroy
+    flash[:success] = "A record has been successfully deleted"
+    redirect_to contracts_path
+    end
+    
 end
