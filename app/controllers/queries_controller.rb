d836e96ffe15865ea9b772953350f73b9978a218
Matteo De Michele <Demi@oa-edu-180-12.wireless.abdn.ac.uk>
2017-08-10 15:36:41 +0100

    files renamed

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,62 +1,65 @@
-class TestsController < ApplicationController
+class QueriesController < ApplicationController
+    # Here is where the ruby code that allows this controller to retrieve all the records related to a specific query is stored
+    # We defined a method for each of the queries that calls a specific query, selects all the records and stores them into a variable
+    # That variable will then be called into a specific view
 
-    def test1
-		require 'pg'
+    def query1
+		  require 'pg'
  		  db = PG.connect(dbname: 'data_scraper1_development', user: 'matteo', password: '')
  	      # HEROKU VARIABLES TO HIDE THE ABOVE SENSITIVE INFO -> TO DO
  		  @function = "Select * FROM Query1('#{params[:operating_company_name]}')"
 		  @companies = db.exec(@function)
     end
 
-    def test2
-    require 'pg'
+    def query2
+      require 'pg'
       @operating_companies=Company.where(:company_type => 'operating')
       db = PG.connect(dbname: 'data_scraper1_development', user: 'matteo', password: '')
       # HEROKU VARIABLES TO HIDE THE ABOVE SENSITIVE INFO -> TO DO
       @assets = db.exec("SELECT * from query2('#{params[:operating]}')")
     end
 
-    def test4
-		require 'pg'
+    def query4
+		  require 'pg'
       @project_titles=Project.all
 		  db = PG.connect(dbname: 'data_scraper1_development', user: 'matteo', password: '')
 		  # HEROKU VARIABLES TO HIDE THE ABOVE SENSITIVE INFO -> TO DO
 		  @contracts = db.exec("SELECT * from query4('#{params[:project_name]}')")
     end
 
-    def test5
-		require 'pg'
+    def query5
+		  require 'pg'
       @stakeholders=Stakeholder.all
 		  db = PG.connect(dbname: 'data_scraper1_development', user: 'matteo', password: '')
 		  # HEROKU VARIABLES TO HIDE THE ABOVE SENSITIVE INFO -> TO DO
 		  @fields = db.exec("SELECT * from query5('#{params[:stakeholder_name]}')")
     end
 
-    def test6
-    require 'pg'
+    def query6
+      require 'pg'
       @stakeholders=Stakeholder.all
       db = PG.connect(dbname: 'data_scraper1_development', user: 'matteo', password: '')
       # HEROKU VARIABLES TO HIDE THE ABOVE SENSITIVE INFO -> TO DO
       @operators = db.exec("SELECT * from query6('#{params[:stakeholder_name]}')")
     end
 
-    def test7
-    require 'pg'
+    def query7
+      require 'pg'
       db = PG.connect(dbname: 'data_scraper1_development', user: 'matteo', password: '')
       # HEROKU VARIABLES TO HIDE THE ABOVE SENSITIVE INFO -> TO DO
       @fields = db.exec("SELECT * from query7('1')")
     end
 
-    def test10
-		require 'pg'
+    def query10
+		  require 'pg'
       @allcompanies=Company.all
 		  db = PG.connect(dbname: 'data_scraper1_development', user: 'matteo', password: '')
 		  # HEROKU VARIABLES TO HIDE THE ABOVE SENSITIVE INFO -> TO DO
 		  @companies = db.exec("SELECT * from query10('#{params[:company_name]}')")
     end
 
-    def test12
-		require 'pg'
+    def query12
+		  require 'pg'
       @operating_companies=Operating.all
 		  db = PG.connect(dbname: 'data_scraper1_development', user: 'matteo', password: '')
 		  # HEROKU VARIABLES TO HIDE THE ABOVE SENSITIVE INFO -> TO DO
@@ -71,8 +74,8 @@ class TestsController < ApplicationController
   #   end
 
     def project
-		require 'pg'
-		  db = PG.connect(dbname: 'data_scraper1_development', user: 'matei', password: 'matei1986')
+		  require 'pg'
+		  db = PG.connect(dbname: 'data_scraper1_development', user: 'matteo', password: '')
 		  # HEROKU VARIABLES TO HIDE THE ABOVE SENSITIVE INFO -> TO DO
 		  @projects = "SELECT * from projects3('#{params[:search]}', '#{params[:search1]}')"
 		  @fields = db.exec(@projects)
