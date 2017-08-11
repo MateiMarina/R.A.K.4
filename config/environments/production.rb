f958eb78691f007ab9ffcbe5704195b9d20b7004
tonyunique2006@yahoo.com <tonyunique2006@yahoo.com>
2017-08-11 10:48:42 +0000

    fresh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
    config.action_mailer.perform_caching = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://murmuring-sands-17291.herokuapp.com/'
+  host = 'https://immense-springs-75866.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
