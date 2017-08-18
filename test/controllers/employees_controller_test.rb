524f0f78ab063b1b53df9ed40d07abbae17cc9ce
tonyunique2006@yahoo.com <tonyunique2006@yahoo.com>
2017-08-18 14:45:04 +0000

    Test

--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -1,20 +1,19 @@
 require 'test_helper'
 
 class EmployeesControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get employees_index_url
-  #   assert_response :success
-  # end
+  
+  test "should data table" do
+    get static_pages_data_tables_path
+    assert_response :success
+  end
 
-  # test "should get new" do
-  #   get new_service_path 
-  #   assert_response :success
-  # end
+#   test "if create in the database" do
+#   assert_no_difference "Employee.count" do
+#   post employees_path employee:  {  full_name: " ", current_company: " ", country: " ",  position: " ", ak_notes: " ", classification: " " }
+#   end
+#   assert_template 'new'
+#   end
 
-  # test "should data table" do
-  #   get static_pages_data_tables_path
-  #   assert_response :success
-  # end
 
 #   test "if not create in the database" do
 #   assert_no_difference "Employee.count" do
--- test/controllers/locations_controller_test.rb
+++ test/controllers/locations_controller_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class LocationsControllerTest < ActionDispatch::IntegrationTest
+ 
  test "should get new" do
-    get new_location_path  
+    get new_location_path
     assert_response :success
   end
 
@@ -12,17 +13,25 @@ class LocationsControllerTest < ActionDispatch::IntegrationTest
   end
 
 
-  test "can create an location" do
+  test "should create a new record" do
   get new_location_path
   assert_response :success
- 
-#   post create,
-#     params: { location: { country: "can create", supra_region: "article successfully."  region:"UK"} }
-
-#   assert_response :success
+  assert_difference "Location.count" do
+  post locations_path location: { country: "can create", supra_region: "article successfully.",  region:"UK"} 
+  end 
+  assert_redirected_to locations_path
+  end 
   
-   end
-end
+  test "if not create in the database" do
+  assert_no_difference "Location.count" do
+  post locations_path location: { country: " ", supra_region: " ",  region:" "} 
+  end
+  assert_template 'new'
+  end 
+  
+end 
 
 
-        
+      
+
+ 
